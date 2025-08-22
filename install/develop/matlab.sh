217d8ccc1f497868443d9a0e55bdf2482547c069
Juan María Herrera López <juanma56herrera@gmail.com>
2025-08-22 20:06:23 +0200

    sc

---
+++ install/develop/matlab.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Download latest MATLAB installer
+#wget -O matlab_installer.zip "https://www.mathworks.com/downloads/web_downloads/download/getRelease?release=R2023a&platform=linux64&mode=release"
